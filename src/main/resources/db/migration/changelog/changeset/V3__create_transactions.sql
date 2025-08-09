e0ffdf69669a8d7e0a1955fc153e0f23cb22fd58
ThisAster <freizen034@mail.ru>
2025-08-09 19:30:02 +0300

    add: repository

--- src/main/resources/db/migration/changelog/changeset/V3__create_transactions.sql
+++ src/main/resources/db/migration/changelog/changeset/V3__create_transactions.sql
@@ -1,5 +1,5 @@
 CREATE TABLE transactions (
-                              id BIGSERIAL PRIMARY KEY,
+                              id SERIAL PRIMARY KEY,
                               from_card_id BIGINT,
                               to_card_id BIGINT,
                               amount DECIMAL(19,2) NOT NULL,
