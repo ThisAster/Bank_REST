d56cb8c51b59574489ee5739092af291203c34dd
ThisAster <freizen034@mail.ru>
2025-08-09 17:09:17 +0300

    edit: entity

--- src/main/java/com/example/bankcards/entity/Card.java
+++ src/main/java/com/example/bankcards/entity/Card.java
@@ -18,10 +18,12 @@ public class Card {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "encrypted_card_number", nullable = false, unique = true)
     private String encryptedCardNumber;
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private User owner;
+    @Column(name = "expiration_date")
     private LocalDateTime expirationDate;
     @Enumerated(EnumType.STRING)
     private CardStatus status;
--- src/main/java/com/example/bankcards/entity/User.java
+++ src/main/java/com/example/bankcards/entity/User.java
@@ -19,7 +19,9 @@ public class User {
 
     @Column(unique = true)
     private String username;
+    @Column(nullable = false)
     private String password;
+    @Column(unique = true, nullable = false)
     private String email;
     @ElementCollection(targetClass = Role.class)
     @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
